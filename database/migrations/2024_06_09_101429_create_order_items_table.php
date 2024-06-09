41781f3c72b9c2e7868dd63201765f3ff0df8e74
vickypandey14 <vickypandeyts@gmail.com>
2024-06-09 22:51:51 +0530

    migration schema created for addresses and order items table

--- database/migrations/2024_06_09_101429_create_order_items_table.php
+++ database/migrations/2024_06_09_101429_create_order_items_table.php
@@ -13,6 +13,11 @@ return new class extends Migration
     {
         Schema::create('order_items', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('order_id')->constrained('orders')->cascadeOnDelete();
+            $table->foreignId('product_id')->constrained('products')->cascadeOnDelete();
+            $table->integer('quantity')->default(1);
+            $table->decimal('unit_amount', 10, 2)->nullable();
+            $table->decimal('total_amount', 10, 2)->nullable();
             $table->timestamps();
         });
     }
--- database/migrations/2024_06_09_101447_create_addresses_table.php
+++ database/migrations/2024_06_09_101447_create_addresses_table.php
@@ -13,6 +13,14 @@ return new class extends Migration
     {
         Schema::create('addresses', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('order_id')->constrained('orders')->cascadeOnDelete();
+            $table->string('first_name')->nullable();
+            $table->string('last_name')->nullable();
+            $table->string('phone')->nullable();
+            $table->longText('street_address')->nullable();
+            $table->string('city')->nullable();
+            $table->string('state')->nullable();
+            $table->string('zip_code')->nullable();
             $table->timestamps();
         });
     }
