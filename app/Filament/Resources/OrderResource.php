78e3a8e87d4edf8e5a46d492ee8a38843896e96b
vickypandey14 <vickypandeyts@gmail.com>
2024-06-13 18:14:35 +0530

    worked on the order resource

--- app/Filament/Resources/OrderResource.php
+++ app/Filament/Resources/OrderResource.php
@@ -5,8 +5,18 @@ namespace App\Filament\Resources;
 use App\Filament\Resources\OrderResource\Pages;
 use App\Filament\Resources\OrderResource\RelationManagers;
 use App\Models\Order;
+use App\Models\Product;
 use Filament\Forms;
+use Filament\Forms\Components\Group;
+use Filament\Forms\Components\Repeater;
+use Filament\Forms\Components\Section;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\Textarea;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\ToggleButtons;
 use Filament\Forms\Form;
+use Filament\Forms\Get;
+use Filament\Forms\Set;
 use Filament\Resources\Resource;
 use Filament\Tables;
 use Filament\Tables\Table;
@@ -23,7 +33,132 @@ class OrderResource extends Resource
     {
         return $form
             ->schema([
-                //
+                Group::make()->schema([
+                    Section::make('Order Information')->schema([
+                        Select::make('user_id')
+                            ->label('Customer')
+                            ->relationship('user', 'name')
+                            ->searchable()
+                            ->preload()
+                            ->required(),
+                        Select::make('Payment Method')
+                            ->options([
+                                'stripe' => 'Stripe',
+                                'code' => 'Cash on Delivery',
+                            ])
+                            ->required(),
+
+                        Select::make('Payment Status')
+                            ->options([
+                                'pending' => 'Pending',
+                                'paid' => 'Paid',
+                                'failed' => 'Failed',
+                            ])
+                            ->default('pending')
+                            ->required(),
+
+                        ToggleButtons::make('status')
+                            ->inline()
+                            ->default('new')
+                            ->options([
+                                'new' => 'New',
+                                'processing' => 'Processing',
+                                'shipped' => 'Shipped',
+                                'delivered' => 'Delivered',
+                                'cancelled' => 'Cancelled',
+                            ])
+                            ->colors([
+                                'new' => 'info',
+                                'processing' => 'warning',
+                                'shipped' => 'success',
+                                'delivered' => 'success',
+                                'cancelled' => 'danger',
+                            ])
+                            ->icons([
+                                'new' => 'heroicon-m-sparkles',
+                                'processing' => 'heroicon-m-arrow-path',
+                                'shipped' => 'heroicon-m-truck',
+                                'delivered' => 'heroicon-m-check-badge',
+                                'cancelled' => 'heroicon-m-x-circle',
+                            ])
+                            ->required(),
+
+                        Select::make('currency')
+                            ->options([
+                                'inr' => 'INR',
+                                'usd' => 'USD',
+                                'EUR' => 'EUR',
+                                'GBP' => 'GBP',
+                            ])
+                            ->default('inr')
+                            ->required(),
+
+                        Select::make('shipping_method')
+                            ->options([
+                                'fedex_ground' => 'FedEx Ground',
+                                'fedex_express' => 'FedEx Express',
+                                'fedex_overnight' => 'FedEx Overnight',
+                                'ups_ground' => 'UPS Ground',
+                                'ups_expedited' => 'UPS Expedited',
+                                'ups_next_day_air' => 'UPS Next Day Air',
+                                'dhl_ground' => 'DHL Ground',
+                                'dhl_express' => 'DHL Express',
+                                'usps_priority_mail' => 'USPS Priority Mail',
+                                'usps_first_class_mail' => 'USPS First Class Mail',
+                                'local_courier' => 'Local Courier',
+                                'bike_messenger' => 'Bike Messenger',
+                            ]),
+
+                        Textarea::make('notes')
+                            ->columnSpanFull()
+                            ->placeholder('Enter a Note...')
+                    ])->columns(2),
+
+                    Section::make('Order Items')->schema([
+                        Repeater::make('items')
+                            ->relationship()
+                            ->schema([
+
+                                Select::make('product_id')
+                                    ->relationship('product', 'name')
+                                    ->searchable()
+                                    ->preload()
+                                    ->required()
+                                    ->distinct()
+                                    ->disableOptionsWhenSelectedInSiblingRepeaterItems()
+                                    ->columnSpan(4)
+                                    ->reactive()
+                                    ->afterStateUpdated(fn ($state, Set $set) => $set('unit_amount', optional(Product::find($state))->price ?? 0))
+
+                                    ->afterStateUpdated(fn ($state, Set $set) => $set('total_amount', optional(Product::find($state))->price ?? 0)),
+
+                                
+                                TextInput::make('quantity')
+                                    ->numeric()
+                                    ->required()
+                                    ->default(1)
+                                    ->minValue(1)
+                                    ->columnSpan(2)
+                                    ->reactive()
+                                    ->afterStateUpdated(fn ($state, Set $set, Get $get) => $set('total_amount', $state*$get('unit_amount'))),
+                                
+                                TextInput::make('unit_amount')
+                                    ->numeric()
+                                    ->required()
+                                    ->disabled()
+                                    ->dehydrated()
+                                    ->columnSpan(3),
+                                
+                                TextInput::make('total_amount')
+                                    ->numeric()
+                                    ->required()
+                                    ->dehydrated()
+                                    ->columnSpan(3)
+
+                            ])->columns(12)
+                    ]),
+
+                ])->columnSpanFull()
             ]);
     }
 
