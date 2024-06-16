07df2cb633a33a69aaa5c65e5a0b730f2ee4d775
vickypandey14 <vickypandeyts@gmail.com>
2024-06-16 23:54:11 +0530

    a frontend page: my orders page is created using livewire

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Livewire\CartPage;
 use App\Livewire\CategoriesPage;
 use App\Livewire\CheckoutPage;
 use App\Livewire\HomePage;
+use App\Livewire\MyOrdersPage;
 use App\Livewire\ProductDetailPage;
 use App\Livewire\ProductsPage;
 use Illuminate\Support\Facades\Route;
@@ -20,6 +21,8 @@ Route::get('/product/{product}', ProductDetailPage::class)->name('product-detail
 Route::get('/cart', CartPage::class)->name('cart-products');
 
 Route::get('/checkout', CheckoutPage::class)->name('checkout');
+
+Route::get('/my-orders', MyOrdersPage::class)->name('my-orders');
 
 // Route::get('/', function () {
 //     return view('welcome');
