10edca15706f5b17707f0fbf41dc39ca145fa907
vickypandey14 <vickypandeyts@gmail.com>
2024-06-20 23:23:16 +0530

    filter products based on categories

--- app/Livewire/ProductsPage.php
+++ app/Livewire/ProductsPage.php
@@ -6,6 +6,7 @@ use App\Models\Brand;
 use App\Models\Category;
 use App\Models\Product;
 use Livewire\Attributes\Title;
+use Livewire\Attributes\Url;
 use Livewire\Component;
 use Livewire\WithPagination;
 
@@ -14,6 +15,9 @@ class ProductsPage extends Component
 {
     use WithPagination;
 
+    #[Url]
+    public $selected_categories = [];
+
     public function render()
     {
         $products = Product::query()->where('is_active', 1);
@@ -21,6 +25,12 @@ class ProductsPage extends Component
         $brands = Brand::where('is_active', 1)->get(['id', 'name', 'slug']);
         
         $categories = Category::where('is_active', 1)->get(['id', 'name', 'slug']);
+
+
+        if(!empty($this->selected_categories))
+        {
+            $products->whereIn('category_id', $this->selected_categories);
+        }
 
         return view('livewire.products-page', [
             'products' => $products->paginate(12),
